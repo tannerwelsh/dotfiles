43b385e207984a7e2dad52d69eb2797603dda825
Tanner Welsh <tannerwelsh@gmail.com>
2024-06-25 12:33:26 -0600

    Add nvim plugins
    - autopairs
    - comment
    - substitute
    - surround

---
+++ configs/nvim/lua/tanner/plugins/surround.lua
@@ -0,0 +1,9 @@
+-- Surround motions
+-- https://github.com/kylechui/nvim-surround
+
+return {
+  "kylechui/nvim-surround",
+  event = { "BufReadPre", "BufNewFile" },
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  config = true,
+}
