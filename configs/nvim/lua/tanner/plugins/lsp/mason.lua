fc5abab1bdee445a8a42eab7aab210283f805a75
Tanner Welsh <tannerwelsh@gmail.com>
2024-07-07 15:28:02 -0600

    Setup nvim linting + formatting
    And add config for lua styles in stylua.toml

--- configs/nvim/lua/tanner/plugins/lsp/mason.lua
+++ configs/nvim/lua/tanner/plugins/lsp/mason.lua
@@ -5,21 +5,24 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim", -- To manage non-LSP tools (like formatters)
   },
   config = function()
     local mason = require("mason")
 
     local mason_lspconfig = require("mason-lspconfig")
 
+    local mason_tool_installer = require("mason-tool-installer")
+
     -- enable mason and configure icons
     mason.setup({
-      -- ui = {
-      --   icons = {
-      --     package_installed = "✓",
-      --     package_pending = "➜",
-      --     package_uninstalled = "✗",
-      --   },
-      -- },
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
     })
 
     mason_lspconfig.setup({
@@ -30,6 +33,14 @@ return {
         "cssls",
         "tailwindcss",
         "lua_ls",
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "eslint_d", -- JS/TS linting (see linting.lua)
       },
     })
   end,
