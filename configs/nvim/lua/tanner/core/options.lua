c9900e7d07d09c285321ad777823f09a50958e8d
Tanner Welsh <tannerwelsh@gmail.com>
2024-07-10 11:51:10 -0600

    Wrap on word breaks

--- configs/nvim/lua/tanner/core/options.lua
+++ configs/nvim/lua/tanner/core/options.lua
@@ -14,6 +14,7 @@ opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
+opt.linebreak = true -- wrap on word breaks, not in the middle of words
 
 -- search / replace settings
 opt.incsearch = true -- highlight search results in real-time
