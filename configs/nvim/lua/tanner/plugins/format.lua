72a6e5412ef2dab247c8e4b79702bc9352f9a62a
Tanner Welsh <tannerwelsh@gmail.com>
2024-07-17 16:16:06 -0600

    Format format.lua

--- configs/nvim/lua/tanner/plugins/format.lua
+++ configs/nvim/lua/tanner/plugins/format.lua
@@ -2,38 +2,42 @@
 -- https://github.com/stevearc/conform.nvim
 
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				graphql = { "prettier" },
-				lua = { "stylua" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
-		})
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        -- to avoid auto-formatting code within code blocks,
+        -- just use "trim_whitespace" instead of default "prettier" formatter
+        -- for more info, see the "injected" formatter options
+        markdown = { "trim_whitespace" },
+        graphql = { "prettier" },
+        lua = { "stylua" },
+        ["_"] = { "trim_whitespace" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
 
-		vim.keymap.set({ "n", "v" }, "<leader>=", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+    vim.keymap.set({ "n", "v" }, "<leader>=", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
 }
