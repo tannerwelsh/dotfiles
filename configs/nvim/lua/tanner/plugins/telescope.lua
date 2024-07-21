c9d5ccddd3732dbf2b8e4a0df902c65b4d21eea7
Tanner Welsh <tannerwelsh@gmail.com>
2024-07-21 13:05:39 -0600

    Add more telescope commands

--- configs/nvim/lua/tanner/plugins/telescope.lua
+++ configs/nvim/lua/tanner/plugins/telescope.lua
@@ -28,10 +28,15 @@ return {
 
     telescope.load_extension("fzf")
 
+    vim.keymap.set("n", "<leader>f*", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffer" })
     vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    vim.keymap.set("n", "<leader>fgc", "<cmd>Telescope git_commits<cr>", { desc = "Find commit in git history" })
+    vim.keymap.set("n", "<leader>fgf", "<cmd>Telescope git_files<cr>", { desc = "Find file tracked by git" })
+    vim.keymap.set("n", "<leader>fj", "<cmd>Telescope jumplist<cr>", { desc = "Find in jumplist" })
+    vim.keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find keymapping" })
+    vim.keymap.set("n", "<leader>fm", "<cmd>Telescope marks<cr>", { desc = "Find marks" })
     vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     vim.keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    vim.keymap.set("n", "<leader>f*", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    vim.keymap.set("n", "<leader>fm", "<cmd>Telescope marks<cr>", { desc = "Find marks" })
   end,
 }
